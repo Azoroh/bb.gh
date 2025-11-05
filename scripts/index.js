2d70236d9293fc464ed2dc9735c46258e7de0969
jay <cjaylambs@gmail.com>
2025-11-05 14:10:18 +0100

    Initial commit - added video autoplay feature

---
+++ scripts/index.js
@@ -0,0 +1,83 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/* Menu show */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* Menu hidden */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav_link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// SWIPER HOME
+const swiperHome = new Swiper(".home_swiper", {
+  loop: true,
+  slidesPerView: "auto",
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  //   autoplay: {
+  //     delay: 2000,
+  //     disableOnInteraction: false,
+  //   },
+});
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    window.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+
+
+
+
+
+
+
+
+
+// AUTO PLAY ON SCROLL
+const video = document.querySelector('.video-container video');
+
+// Fade in when the video starts playing
+video.addEventListener('playing', () => {
+  video.classList.add('loaded');
+});
+
+// Autoplay when visible
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+}, { threshold: 0.5 });
+
+observer.observe(video);
